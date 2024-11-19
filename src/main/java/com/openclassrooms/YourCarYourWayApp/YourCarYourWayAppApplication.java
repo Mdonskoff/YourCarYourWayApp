6dd10719e5677d76006f85aacc96f5782896f2fc
Marushka LABORDE-DONSKOFF <mdonskoff@mbp-de-marushka.home>
2024-11-19 17:13:00 +0100

    First commit

---
+++ src/main/java/com/openclassrooms/YourCarYourWayApp/YourCarYourWayAppApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.YourCarYourWayApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourCarYourWayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourCarYourWayAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/YourCarYourWayApp/YourCarYourWayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.YourCarYourWayApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourCarYourWayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
